3af50621660896277b104db47293c0b96624527d
Denis <dmzainullin@yandex.ru>
2022-08-21 18:03:46 +0500

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String name, food;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void showNick(String nick) {
+        this.name = nick;
+    }
+
+    public  void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Имя котика и что он ел: ");
+        Cat gav = new Cat();
+        gav.showNick("Гав");
+        gav.eat("Котлета");
+        gav.show();
+        System.out.println("Имя котика и что он ел: ");
+        Cat black = new Cat();
+        black.showNick("Черныш");
+        black.eat("Рыба");
+        black.show();
+    }
+}
