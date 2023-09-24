e2be0ff4f76302887862c493f177cda026e722ba
Denis <dmzainullin@yandex.ru>
2023-09-24 20:26:28 +0500

    4. Переопределение метода [#174890 #445813]

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -1,6 +1,6 @@
 package ru.job4j.inheritance;
 
-public class TexttReport {
+public class TextReport {
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
