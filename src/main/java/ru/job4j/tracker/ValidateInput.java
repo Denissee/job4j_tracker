da97fbc5d028314a51c1e413ee7eaeb56f3bb7ac
Denis <dmzainullin@yandex.ru>
2024-01-28 00:30:47 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -10,7 +10,7 @@ public class ValidateInput implements Input {
     }
 
     @Override
-    public String askStr(String question){
+    public String askStr(String question) {
         return input.askStr(question);
     }
 
